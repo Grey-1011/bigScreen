f7aee802d6601e7d5af159c9faa38226e9e42314
grey <z1251543107@gmail.com>
2022-08-22 20:37:12 +0800

    add px function  add header

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    Hello Home
+  <div class="home">
+    <header></header>
   </div>
 </template>
+
+<script lang="ts" setup>
+
+
+</script>
+
+<style lang="scss">
+@import "../shared/helper.scss";
+
+  .home {
+    header {
+      height: px(99);
+      background-image: url("../assets/images/header.png");
+      background-size: cover;
+    }
+  }
+</style>
