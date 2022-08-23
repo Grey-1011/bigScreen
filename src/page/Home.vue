7a0e3633633660c90b3087ebe06757e73e96d6ae
grey <z1251543107@gmail.com>
2022-08-24 00:31:13 +0800

    制做 Chart5 表格 table

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -10,7 +10,9 @@
         <Chart3/>
         <Chart4/>
       </section>
-      <section class="section3 bordered"></section>
+      <section class="section3 bordered">
+        <Chart5/>
+      </section>
       <section class="section4 bordered"></section>
       <section class="section5 bordered"></section>
     </main>
@@ -24,6 +26,7 @@ import Chart1 from '../components/Chart1.vue';
 import Chart2 from '../components/Chart2.vue';
 import Chart3 from '../components/Chart3.vue';
 import Chart4 from '../components/Chart4.vue';
+import Chart5 from '../components/Chart5.vue';
 </script>
 
 <style lang="scss">
